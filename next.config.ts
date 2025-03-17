73c3be81821ad573b8bcbc95038b30fac0cb4f25
lilachbitton <lilacbra@gmail.com>
2025-03-17 09:49:16 +0200

    next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // תוספות אופציונליות אחרות שיש לך
 };
 
-export default nextConfig;
+module.exports = nextConfig;
